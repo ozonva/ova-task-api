c1f5e52c059bc8f13e5daca5eb671e702bcd2de0
Pavel Burenev <burenevpn@gmail.com>
2021-08-10 23:40:00 +0300

    task #2 add tests for panic

--- internal/utils/map_inverter_test.go
+++ internal/utils/map_inverter_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
 )
@@ -15,16 +16,26 @@ func TestInvertMap(t *testing.T) {
 		want map[string]string
 	}{
 		{"empty map", args{source: map[string]string{}}, map[string]string{}},
-		{"non empty map", args{source: map[string]string{"key1" : "value1", "key2": "value2"}}, map[string]string{"value1" : "key1", "value2": "key2"}},
-
-		// todo need to test panic
-		//{"map with duplicated values", args{source: map[string]string{"1": "1", "2": "1"}}, map[string]string{"1": "1"}},
+		{"non empty map", args{source: map[string]string{"key1": "value1", "key2": "value2"}}, map[string]string{"value1": "key1", "value2": "key2"}},
+	}
+	panicTests := []struct {
+		name string
+		args args
+	}{
+		{"map with duplicated values", args{source: map[string]string{"1": "1", "2": "1"}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := InvertMap(tt.args.source); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("InvertMap() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+	for _, tt := range panicTests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Panicsf(t, func() {
+				InvertMap(tt.args.source)
+			}, "Method should panic")
 		})
 	}
 }
--- internal/utils/slice_splitter_test.go
+++ internal/utils/slice_splitter_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
 )
@@ -15,9 +16,6 @@ func TestSplitSlice(t *testing.T) {
 		args args
 		want [][]string
 	}{
-		// todo need to test panic
-		//{"empty source, batch = 0", args{source: []string{}, batch: 0}, [][]string{}},
-		//{"non empty source, batch = 0", args{source: []string{}, batch: 0}, [][]string{}},
 		{"empty source, batch > 0", args{source: []string{}, batch: 5}, [][]string{}},
 		{"empty source, batch > 0", args{source: nil, batch: 5}, [][]string{}},
 		{"non empty source, batch = 1", args{source: []string{"1", "2", "3"}, batch: 1}, [][]string{{"1"}, {"2"}, {"3"}}},
@@ -26,6 +24,13 @@ func TestSplitSlice(t *testing.T) {
 		{"non empty source, batch > len(source)/2", args{source: []string{"1", "2", "3", "4", "5"}, batch: 3}, [][]string{{"1", "2", "3"}, {"4", "5"}}},
 		{"non empty source, batch = len(source)", args{source: []string{"1", "2", "3"}, batch: 3}, [][]string{{"1", "2", "3"}}},
 		{"non empty source, batch > len(source)", args{source: []string{"1", "2", "3"}, batch: 5}, [][]string{{"1", "2", "3"}}},
+	}
+	panicTests := []struct {
+		name string
+		args args
+	}{
+		{"empty source, batch = 0", args{source: []string{}, batch: 0}},
+		{"non empty source, batch = 0", args{source: []string{}, batch: 0}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -36,6 +41,13 @@ func TestSplitSlice(t *testing.T) {
 			if !reflect.DeepEqual(slices, tt.want) {
 				t.Errorf("actual = %v, expected %v", slices, tt.want)
 			}
+		})
+	}
+	for _, tt := range panicTests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Panicsf(t, func() {
+				SplitSlice(tt.args.source, tt.args.batch)
+			}, "Method should panic")
 		})
 	}
 }
