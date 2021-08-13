e4e41d1be93a560bbf2b24de4123320b049ab551
Pavel Burenev <burenevpn@gmail.com>
2021-08-13 10:45:16 +0300

    task #3 config cycling reading

---
+++ internal/utils/configReader.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+)
+
+type configuration struct {
+	ConfigVersion string `json:"configVersion"`
+}
+
+func ConfigCyclicReading(configFilePath string, configUpdatePeriodSeconds int, configUpdateHandle func(key string)) {
+	var workingVersion string
+	for {
+		config := readConfig(configFilePath)
+		actualVersion := config.ConfigVersion
+		if actualVersion != workingVersion {
+			workingVersion = actualVersion
+			configUpdateHandle(actualVersion)
+		}
+		time.Sleep(time.Duration(configUpdatePeriodSeconds) * time.Second)
+	}
+}
+
+func readConfig(configFilePath string) configuration {
+	file, _ := os.Open(configFilePath)
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+	decoder := json.NewDecoder(file)
+	configuration := configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("config decode error:", err)
+	}
+	return configuration
+}
