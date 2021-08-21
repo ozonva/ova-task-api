885421f0ee879d9dadee582b16f47becac0c8b2b
Pavel Burenev <burenevpn@gmail.com>
2021-08-21 07:08:43 +0300

    task #4 suite test for flusher

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.16
 require github.com/stretchr/testify v1.7.0
 
 require (
-	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/onsi/ginkgo v1.16.4 // indirect
+	github.com/onsi/gomega v1.15.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- internal/flusher/flusher.go
+++ internal/flusher/flusher.go
@@ -20,18 +20,24 @@ func New(
 	chunkSize int,
 	entityRepo repo.Repo,
 ) Flusher {
+	if entityRepo == nil {
+		panic("entityRepo is nil")
+	}
+	if chunkSize <= 0 {
+		panic("chunkSize must be greater than 0")
+	}
 	return &flusher{
 		chunkSize:  chunkSize,
 		entityRepo: entityRepo,
 	}
 }
 
-func (flusher *flusher) Flush(entities []tasks.Task) []tasks.Task {
-	notFlushed := make([]tasks.Task, 0, 0)
+func (flusher *flusher) Flush(entities []tasks.Task) (notFlushed []tasks.Task) {
+	notFlushed = make([]tasks.Task, 0, 0)
 	for _, chunk := range utils.SplitTasksSlice(entities, flusher.chunkSize) {
 		err := flusher.entityRepo.AddEntities(chunk)
 		if err != nil {
-			fmt.Println("chunk not flushed ", err)
+			fmt.Println("chunk not flushed", err)
 			notFlushed = append(notFlushed, chunk...)
 		}
 	}
